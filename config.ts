c70b0aeae49cd034533bcdd9de903649f7ea9909
nntvi <vietnamblockchain.asia>
2024-12-10 17:19:44 +0700

    SEO meta data favicon, ...

--- config.ts
+++ config.ts
@@ -2,12 +2,12 @@ import { z } from "zod";
 
 const configSchema = z.object({
   NEXT_PUBLIC_API_ENDPOINT: z.string(),
-  NEXT_PUBLIC_API: z.string(),
+  // NEXT_PUBLIC_API: z.string(),
 });
 
 const configProject = configSchema.safeParse({
   NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
-  NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
+  // NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
 });
 if (!configProject.success) {
   console.log(process.env);
