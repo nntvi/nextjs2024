d9eb7096b591a144d3aa321cfb00a67a99c6eb7b
nntvi <vietnamblockchain.asia>
2024-12-25 17:45:37 +0700

    down size bundle

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -12,4 +16,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
